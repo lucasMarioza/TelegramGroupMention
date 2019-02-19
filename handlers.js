415932d41e06157e5d0733c4565c57701b5bf48f
Alexandre Cesar <acmorcego@gmail.com>
2019-02-19 14:57:51 -0300

    abstract the result handling mechanism

--- handlers.js
+++ handlers.js
@@ -4,6 +4,11 @@ const slimbot = new Slimbot(process.env["BOT_KEY"])
 
 const Handler = (command, handler) => ({ command, handler })
 
+const Message = text => ({ text })
+const Reply = text => ({ text, reply: true })
+const Failure = text => ({ text, failure: true })
+const Empty = () => ({})
+
 function cleanMentionName(mention) {
   return mention.replace(/^@*/g, "")
 }
@@ -17,14 +22,9 @@ const handlers = [
     let [_, mention] = separator(message.text)
     mention = cleanMentionName(mention)
     let user = message.from.username
-    if (commands.createMention(mention, user)) {
-      slimbot.sendMessage(message.chat.id, `Mention @${mention} created.`)
-    } else {
-      slimbot.sendMessage(
-        message.chat.id,
-        `Mention @${mention} already exists.`
-      )
-    }
+    if (commands.createMention(mention, user))
+      return Message(`Mention @${mention} created.`)
+    else return Failure(`Mention @${mention} already exists.`)
   }),
   Handler(/^\/enter[ _]/, async message => {
     let [_, mention, ...users] = separator(message.text)
@@ -39,19 +39,15 @@ const handlers = [
       if (user === undefined) return
     }
 
-    for (user of users) {
-      if (commands.assignToMention(mention, user)) {
-        slimbot.sendMessage(
-          message.chat.id,
-          `user @${user} assigned to @${mention}.`
-        )
-      } else {
-        slimbot.sendMessage(
-          message.chat.id,
-          `Mention @${mention} does not exists. Use /create to create.`
-        )
-      }
-    }
+    const results = users.map(user => commands.assignToMention(mention, user))
+    if (results.includes(false))
+      return Failure(
+        `Mention @${mention} does not exists. Use /create to create.`
+      )
+    if (users.length === 1)
+      return Message(`user @${users[0]} assigned to @${mention}.`)
+    const usersString = users.map(user => `@${user}`).join(" ")
+    return Message(`users ${usersString} assigned to @${mention}.`)
   }),
   Handler(/^\/exit[ _]/, async message => {
     let [_, mention, ...users] = separator(message.text)
@@ -66,76 +62,63 @@ const handlers = [
       if (user === undefined) return
     }
 
-    for (user of users) {
-      if (commands.unassign(mention, user)) {
-        slimbot.sendMessage(
-          message.chat.id,
-          `user @${user} unassigned of @${mention}.`
-        )
-      } else {
-        slimbot.sendMessage(
-          message.chat.id,
-          `Mention @${mention} does not exists or you already wasn't assigned into it.`
-        )
-      }
-    }
+    const results = users.map(user => commands.unassign(mention, user))
+    if (results.includes(false))
+      return Failure(
+        `Mention @${mention} does not exists or you already wasn't assigned into it.`
+      )
+    if (users.length === 1)
+      return Message(`user @${users[0]} unassigned of @${mention}.`)
+    const usersString = users.map(user => `@${user}`).join(" ")
+    return Message(`users ${usersString} unnasigned of @${mention}.`)
   }),
   Handler(/^\/delete[ _]/, message => {
     let [_, mention] = separator(message.text)
     mention = cleanMentionName(mention)
     if (commands.deleteMention(mention))
-      slimbot.sendMessage(message.chat.id, `Mention @${mention} deleted.`)
-    else
-      slimbot.sendMessage(message.chat.id, `Mention @${mention} doesn't exist.`)
+      return Message(`Mention @${mention} deleted.`)
+    else return Failure(`Mention @${mention} doesn't exist.`)
   }),
   Handler(/^\/mentions/, message => {
     let [_, user] = separator(message.text)
     if (user === "me") user = message.from.username
 
     let response = commands.getAllMentions(user).trim()
-    if (response !== "") {
-      slimbot.sendMessage(message.chat.id, response, {
-        reply_to_message_id: message.message_id
-      })
-    } else {
-      slimbot.sendMessage(
-        message.chat.id,
-        "No mentions created for this group yet."
-      )
-    }
+    if (response !== "") return Reply(response)
+    else return Failure("No mentions created for this group yet.")
   }),
   Handler(/^\@admins/, async message => {
     const admins = await slimbot.getChatAdministrators(message.chat.id)
     const usernames = admins.result.map(({ user: { username } }) => username)
     const response = usernames.map(username => `@${username}`).join(" ")
-    if (response !== "")
-      slimbot.sendMessage(message.chat.id, response, {
-        reply_to_message_id: message.message_id
-      })
+    if (response !== "") return Reply(response)
+    return Empty()
   }),
   Handler(/^\@.+/, message => {
     let [mention] = separator(message.text)
     mention = cleanMentionName(mention)
     let response = commands.getMention(mention, message.from.username).trim()
-    if (response !== "")
-      slimbot.sendMessage(message.chat.id, response, {
-        reply_to_message_id: message.message_id
-      })
+    if (response !== "") return Reply(response)
+    return Empty()
   }),
   Handler(/^\/list[ _]/, message => {
     let [_, mention] = separator(message.text)
     let response = commands.getMentionMembers(mention).trim()
-    if (response !== "")
-      slimbot.sendMessage(message.chat.id, response, {
-        reply_to_message_id: message.message_id
-      })
+    if (response !== "") return Reply(response, message.message_id)
+    return Empty()
   })
 ]
 
-const handleMessage = message => {
+const handleMessage = async message => {
   message.text = message.text.replace(/\s{1,}/g, " ")
   const match = handlers.find(({ command }) => command.test(message.text))
-  if (match !== undefined) match.handler(message)
+  if (match !== undefined) {
+    const { text, reply } = await match.handler(message)
+    if (text) {
+      const extra = reply ? { reply_to_message_id: message.message_id } : null
+      slimbot.sendMessage(message.chat.id, text, extra)
+    }
+  }
 }
 
 module.exports = handleMessage
