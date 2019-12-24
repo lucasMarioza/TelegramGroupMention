f277288393f3fd786dddfd6d195ee1798c393ed5
Alexandre Cesar <acmorcego@gmail.com>
2019-12-24 11:31:24 -0300

    update dependencies and run prettier

--- src/textToCommand.js
+++ src/textToCommand.js
@@ -1,12 +1,12 @@
 function separator(text) {
-  const [command, ...parameters] = text.split(" ");
-  return [...command.replace("_", " ").split(" "), ...parameters];
+  const [command, ...parameters] = text.split(" ")
+  return [...command.replace("_", " ").split(" "), ...parameters]
 }
 function textToCommand(text) {
-  if (text[0] != "@" && text[0] != "/") return { invalid: true };
-  let [name, ...params] = separator(text);
-  if (name.match(/^@/)) return { name: "@", params: [name.substr(1)] };
-  if (name.match(/^\//)) return { name: name.substr(1), params };
+  if (text[0] != "@" && text[0] != "/") return { invalid: true }
+  let [name, ...params] = separator(text)
+  if (name.match(/^@/)) return { name: "@", params: [name.substr(1)] }
+  if (name.match(/^\//)) return { name: name.substr(1), params }
 }
 
-module.exports = textToCommand;
+module.exports = textToCommand
