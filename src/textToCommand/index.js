a45c028201fd8810515c8c80b18776b78dc42919
Alexandre Cesar <acmorcego@gmail.com>
2019-12-24 11:54:14 -0300

    fix empty command bug

--- src/textToCommand/index.js
+++ src/textToCommand/index.js
@@ -1,12 +1,16 @@
+const invalid = { invalid: true }
+
 function separator(text) {
   const [command, ...parameters] = text.split(" ")
   return [...command.replace("_", " ").split(" "), ...parameters]
 }
+
 function textToCommand(text) {
-  if (text[0] != "@" && text[0] != "/") return { invalid: true }
+  if (text[0] != "@" && text[0] != "/") return invalid
   let [name, ...params] = separator(text)
-  if (name.match(/^@/)) return { name: "@", params: [name.substr(1)] }
-  if (name.match(/^\//)) return { name: name.substr(1), params }
+  if (name.substr(1) === "") return invalid
+  if (name[0] === "@") return { name: "@", params: [name.substr(1)] }
+  if (name[0] === "/") return { name: name.substr(1), params }
 }
 
 module.exports = textToCommand
